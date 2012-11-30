9efd6820616186c8b968f6257d849b52200fe659
alexisd61@gmail.com <alexisd61@gmail.com>
2012-11-30 15:51:07 +0100

    at question 12

--- src/main/java/chatapp/components/ChatBoard.java
+++ src/main/java/chatapp/components/ChatBoard.java
@@ -4,10 +4,28 @@
  */
 package chatapp.components;
 
+import chatapp.model.ChatMessage;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author alexis
  */
+
+
 public class ChatBoard {
+    
+    private LinkedList<ChatMessage> messages = new LinkedList();
+
+
+    public void add(ChatMessage message) {
+        messages.add(message);
+    }
+    
+    public LinkedList messagesList() {
+    
+        return messages;
+    }
     
 }
--- src/main/java/chatapp/presentation/BoardServlet.java
+++ src/main/java/chatapp/presentation/BoardServlet.java
@@ -4,10 +4,25 @@
  */
 package chatapp.presentation;
 
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ejb.*;
+import chatapp.model.Participant;
+import chatapp.components.ChatBoard;
 /**
  *
  * @author alexis
  */
-public class BoardServlet {
+
+@WebServlet(urlPatterns = "/board")
+
+public class BoardServlet extends HttpServlet{
+    
+    @Inject Participant particpant;
+    @EJB ChatBoard chatboard;
     
 }
