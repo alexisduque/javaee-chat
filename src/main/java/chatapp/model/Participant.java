597d8729fbaee553ff6f3e429862834e3846fa6a
Alexis DUQUE <alexisd61@gmail.com>
2012-11-30 14:15:58 +0100

    first commit !

---
+++ src/main/java/chatapp/model/Participant.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp.model;
+
+/**
+ *
+ * @author alexis
+ */
+public class Participant {
+    private String myName;
+    private String myEmail;
+    
+    public Participant () {
+        myName ="N/A";
+        myEmail = "N/A";
+    }
+
+}
